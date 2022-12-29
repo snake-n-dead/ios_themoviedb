7b246b5c751ccf1ac8a3656bbdce8f64da49d972
Владимир Васильев <revolage.v.vas@gmail.com>
2022-12-29 13:53:55 +0600

    Project initialization

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Vladimir Vasilyev on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
